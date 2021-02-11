e17dfda70753eef11bfc3c266c2fe5ed80f7f59a
Luke Truitt <luke@truittluke.com>
2021-02-10 19:59:48 -0700

    dfa

--- src/components/account/AccountPage.js
+++ src/components/account/AccountPage.js
@@ -160,6 +160,9 @@ function AccountTimeline(props) {
   );
 }
 function numberWithCommas(x) {
+  if(x==null) {
+    return "";
+  }
   var parts = x.toString().split(".");
   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   return parts.join(".");
